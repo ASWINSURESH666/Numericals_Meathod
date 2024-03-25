33eb97319a067cd3d3fe1c3b5c6748906b5c5ff0
Aswin_SuresH <117917680+ASWINSURESH666@users.noreply.github.com>
2024-03-25 15:20:38 +0530

    Create 10. Simpson's 1_by_3 Rule.py

---
+++ 10. Simpson's 1_by_3 Rule.py
@@ -0,0 +1,16 @@
+#  Simpson's 1/8 rule
+
+def simpson1_3(x, y):
+
+    h = x[1] - x[0]
+    middle_term_4 = sum(y[i] for i in range(1, len(y) - 1, 2))
+    middle_term_2 = sum(y[i] for i in range(2, len(y) - 1, 2))
+    result = (h / 3) * (y[0] + 4 * middle_term_4 + 2 * middle_term_2 + y[-1])
+    return result
+
+# User input
+x = list(map(float, input("Enter the values of x (space-separated): ").split()))
+y = list(map(float, input("Enter the values of y (space-separated): ").split()))
+
+# Output
+print("Approximation of the integral:", simpson1_3(x, y))
